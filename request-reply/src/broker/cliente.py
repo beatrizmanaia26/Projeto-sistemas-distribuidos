38075992412eb15562a5f91247c27d119a0a7088
Nuno Martins <145809068+nunomgs136@users.noreply.github.com>
2026-03-15 03:01:29 +0000

    Comunicação de Cliente Python com Servidor Python funcionado. Checar comunicação com Java e salvamento em arquivo.

--- request-reply/src/broker/cliente.py
+++ request-reply/src/broker/cliente.py
@@ -1,46 +1,54 @@
-""""
 import zmq
 from time import sleep
 import json #eniar json do cliente p servidor
-
-
+from datetime import datetime
+import msgpack
+import zoneinfo
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://broker:5555")
-
+fuso = zoneinfo.ZoneInfo("America/Sao_Paulo")
 i = 0
 while True:
-    msg= json.dumps({"fazer": "criar", "msg":"eba"})
+
+    msg= msgpack.packb({"fazer": "logar", "msg":"loba", "timestamp": datetime.now(tz=fuso).isoformat()})
     print(f"Mensagem {i}:", end=" ", flush=True)#imprimir no terminal
-    socket.send_string(msg)#envia pro servidor
-    mensagem = socket.recv_string()#rcebe do servidor
-    print(f"{mensagem}")
+    socket.send(msg)#envia pro servidor
+    mensagem = socket.recv()#rcebe do servidor
+    print(f"{msgpack.unpackb(mensagem)}")
+    sleep(0.5)
+
+
+    msg= msgpack.packb({"fazer": "criar", "msg":"eba", "timestamp": datetime.now(tz=fuso).isoformat()})
+    print(f"Mensagem {i}:", end=" ", flush=True)#imprimir no terminal
+    socket.send(msg)#envia pro servidor
+    mensagem = socket.recv()#rcebe do servidor
+    print(f"{msgpack.unpackb(mensagem)}")
     sleep(0.5)
 
     #para mandar mais mensagens:
-    msg= json.dumps({"fazer": "remover", "msg":"eba2"})
+    msg= msgpack.packb({"fazer": "remover", "msg":"eba2", "timestamp": datetime.now(tz=fuso).isoformat()})
     print(f"Mensagem {i}:", end=" ", flush=True)#imprimir no terminal
-    socket.send_string(msg)#envia pro servidor
-    mensagem = socket.recv_string()#rcebe do servidor
-    print(f"{mensagem}")
+    socket.send(msg)#envia pro servidor
+    mensagem = socket.recv()#rcebe do servidor
+    print(f"{msgpack.unpackb(mensagem)}")
     sleep(0.5)
     
     
     #para mandar mais mensagens:
-    msg= json.dumps({"fazer": "criar", "msg":"eba3"})
+    msg= msgpack.packb({"fazer": "criar", "msg":"eba3", "timestamp": datetime.now(tz=fuso).isoformat()})
     print(f"Mensagem {i}:", end=" ", flush=True)#imprimir no terminal
-    socket.send_string(msg)#envia pro servidor
-    mensagem = socket.recv_string()#rcebe do servidor
-    print(f"{mensagem}")
+    socket.send(msg)#envia pro servidor
+    mensagem = socket.recv()#rcebe do servidor
+    print(f"{msgpack.unpackb(mensagem)}")
     sleep(0.5)
     
 
     #para mandar mais mensagens:
-    msg= json.dumps({"fazer": "listar", "msg":"eba4"})
+    msg= msgpack.packb({"fazer": "listar", "msg":"eba4", "timestamp": datetime.now(tz=fuso).isoformat()})
     print(f"Mensagem {i}:", end=" ", flush=True)#imprimir no terminal
-    socket.send_string(msg)#envia pro servidor
-    mensagem = socket.recv_string()#rcebe do servidor
-    print(f"{mensagem}")
+    socket.send(msg)#envia pro servidor
+    mensagem = socket.recv()#rcebe do servidor
+    print(f"{msgpack.unpackb(mensagem)}") #Print sem ter os bytes da mensagem
     sleep(0.5)
     i += 1
-"""
--- request-reply/src/broker/servidor.py
+++ request-reply/src/broker/servidor.py
@@ -1,49 +1,75 @@
-"""
+
 import json
 import zmq
-
+import zoneinfo
+from datetime import datetime
+import msgpack
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.connect("tcp://broker:5556")
-
+fuso = zoneinfo.ZoneInfo("America/Sao_Paulo")
 tarefas = list() #lista de tarefas para o servidor processar, add remove pela lista
-
+usuarios = list()
+logins = list()
+logado = False
 while True:
-    message = socket.recv_string() #recebe mensagem do cliente (no REP sempre começa com recv)
-    msg = json.loads(message) #converte a string recebida em um dicionário
+    message = socket.recv() #recebe mensagem do cliente (no REP sempre começa com recv)
+    msg = msgpack.unpackb(message) #converte a string recebida em um dicionário
     fazer = msg["fazer"] #pega o valor da chave "fazer" do dicionário
-
+    #tempo = msg["timestamp"]
+    resposta = ""
     # No padrão REQ/REP do ZeroMQ é OBRIGATÓRIO:
     # REP -> recv -> send -> recv -> send ...
     # Ou seja, SEMPRE depois de um recv precisamos dar um send.
     # Se não enviar resposta, o socket entra em estado inválido e dá erro.
-
-    if fazer == "criar":
-        tarefas.append(msg["msg"]) #adiciona a mensagem do cliente na lista de tarefas
-        resposta = f"Tarefa '{msg['msg']}' criada."
-        socket.send_string(resposta) #envia resposta obrigatória
-        print(resposta, flush=True)
-
-    elif fazer == "remover":
-        if msg["msg"] in tarefas: #verifica se a mensagem do cliente está na lista de tarefas
-            tarefas.remove(msg["msg"]) #remove da lista
-            resposta = f"Tarefa '{msg['msg']}' removida."
+    if fazer == "logar":
+        if msg["msg"] in usuarios:
+            tempo = msg["timestamp"]
+            horario = datetime.now(tz=fuso).isoformat()
+            resposta = f"Login realizado com sucesso às {horario}"
+            login = (msg["msg"], horario)
+            logins.append(login)
+            #socket.send_string(resposta)
+            logado = True
+            print(resposta, flush=True)
+            
         else:
-            # Mesmo se não encontrar a tarefa, PRECISA responder
-            resposta = f"Tarefa '{msg['msg']}' não encontrada."
+            usuarios.append(msg["msg"])
+            resposta = "Cadastro inexistente. Adicionado a lista de usuários..."
+            #socket.send_string(resposta)
+            print(resposta, flush=True)
+            
+    elif logado == True:
+        if fazer == "criar":
+            tarefas.append(msg["msg"] + "|" + tempo) #adiciona a mensagem do cliente na lista de tarefas
+            resposta = f"Tarefa '{msg['msg']}' criada."
+           # socket.send_string(resposta) #envia resposta obrigatória
+            print(resposta, flush=True)
 
-        socket.send_string(resposta) #sempre enviar resposta
-        print(resposta, flush=True)
+        elif fazer == "remover":
+            if msg["msg"] in tarefas: #verifica se a mensagem do cliente está na lista de tarefas
+                tarefas.remove(msg["msg"]) #remove da lista
+                resposta = f"Tarefa '{msg['msg']}' removida às {datetime.now(tz=fuso).isoformat()}."
+            else:
+                # Mesmo se não encontrar a tarefa, PRECISA responder
+                resposta = f"Tarefa '{msg['msg']}' não encontrada às {datetime.now(tz=fuso).isoformat()} ."
 
-    elif fazer == "listar":
-        print(f"Lista de tarefas: {tarefas}", flush=True)
-        resposta = json.dumps(tarefas) #transforma lista em string json
-        socket.send_string(resposta) #sempre enviar resposta
+           # socket.send_string(resposta) #sempre enviar resposta
+            print(resposta, flush=True)
 
+        elif fazer == "listar":
+            print(f"Lista de tarefas: {tarefas}", flush=True)
+            resposta = msgpack.packb(tarefas) #transforma lista em string json
+            #socket.send_string(resposta) #sempre enviar resposta
+
+        else:
+            # Caso inesperado também precisa responder
+            resposta = "Comando inválido."
+            #socket.send_string(resposta)
     else:
-        # Caso inesperado também precisa responder
-        resposta = "Comando inválido."
-        socket.send_string(resposta)
-
-    print(f"Mensagem recebida: {message}", flush=True)
-    """
+        resposta = "Usuário não logado. Faça login primeiro!"
+        # socket.send_string(resposta)
+        print(resposta, flush = True)
+    print(f"Mensagem recebida: {msg} às {datetime.now(tz=fuso)}", flush=True)
+    resp = msgpack.packb(resposta)
+    socket.send(resp)
